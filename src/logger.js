8c3575f5376488ab068d391b6c7980160bc2f597
Westixy <westixy@gmail.com>
2018-06-18 23:58:43 +0200

    change date format

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,13 @@
 module.exports = m => {
   const d = new Date
+  const p = n => String(n).padStart(2, '0')
+  const s = n => String(n).slice(2, 4)
   console.log(`${
-    d.getUTCFullYear()}.${
-    d.getUTCMonth()}.${
-    d.getUTCDate()}-${
-    d.getUTCHours()}:${
-    d.getUTCMinutes()} ${
+    s(d.getUTCFullYear())}/${
+    p(d.getUTCMonth())}/${
+    p(d.getUTCDate())} ${
+    p(d.getUTCHours())}:${
+    p(d.getUTCMinutes())} ${
     m.guild
       ? m.guild.name
       : 'MP'}:${
