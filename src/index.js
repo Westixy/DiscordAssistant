11eb34b5d036ab7c1cfde616de8090c11ef278ec
Westixy <westixy@gmail.com>
2018-06-18 23:00:24 +0200

    fi bug on log

--- src/index.js
+++ src/index.js
@@ -8,10 +8,20 @@ const evaluator = require('./commands/evaluator')
 const bot = new Discord.Client
 
 bot.on('ready', () => console.log('bot:main', 'Bot ready'))
-bot.on('message', m => console.log(`${m.guild.name}:${m.channel.name}:${m.member.displayName} > ${m.content}`))
+bot.on('message', m =>
+  console.log(`${
+    m.guild
+      ? m.guild.name
+      : 'MP'}:${
+    m.channel.type !== 'dm'
+      ? m.channel.name
+      : m.channel.recipient.username}:${
+    m.member
+      ? m.member.displayName
+      : m.author.username} > ${m.content}`))
 
 bot.on('message', m => {
-  if (m.author.tag !== m.guild.me.user.tag) return
+  if (m.author.tag !== m.client.user.tag) return
   reference(m)
   evaluator(m)
 })
